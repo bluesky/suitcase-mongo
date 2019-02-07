eade07b844aa296e08637927d9a30b42097203ac
Dan Allan <dallan@bnl.gov>
2019-02-07 17:28:09 -0500

    Update test requirements.

--- suitcase/mongo_layout1/tests/conftest.py
+++ suitcase/mongo_layout1/tests/conftest.py
@@ -2,4 +2,21 @@ from bluesky.tests.conftest import RE  # noqa
 from ophyd.tests.conftest import hw  # noqa
 from suitcase.utils.tests.conftest import (  # noqa
     example_data, generate_data, plan_type, detector_list, event_type)
-from .fixtures import db_factory  # noqa
+import pymongo
+import pytest
+import uuid
+
+
+@pytest.fixture()
+def db_factory(request):
+    def inner():
+        database_name = f'test-{str(uuid.uuid4())}'
+        uri = f'mongodb://localhost:27017/{database_name}'
+        client = pymongo.MongoClient(uri)
+
+        def drop():
+            client.drop_database(database_name)
+
+        request.addfinalizer(drop)
+        return client
+    return inner
--- suitcase/mongo_layout1/tests/tests.py
+++ suitcase/mongo_layout1/tests/tests.py
@@ -7,8 +7,8 @@ from suitcase.mongo_layout1 import Serializer
 
 def test_export(db_factory, example_data):
     documents = example_data()
-    metadatastore_db = db_factory()
-    asset_registry_db = db_factory()
-    serializer = Serializer(metadatastore_db, asset_registry_db)
+    metadatastore_client = db_factory()
+    asset_registry_client = db_factory()
+    serializer = Serializer(metadatastore_client, asset_registry_client)
     for item in documents:
         serializer(*item)
